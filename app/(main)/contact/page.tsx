5587d165108628da0374cb15c8fd8902efbbd9e0
rodgerbs <rodgerbs31@gmail.com>
2025-11-06 16:55:12 +0000

    test

--- app/(main)/contact/page.tsx
+++ app/(main)/contact/page.tsx
@@ -4,6 +4,9 @@ import { GoogleWebResult } from "@/components/google-components/google-webresult
 //Constants
 import { contactLinks } from "@/constants/contact-links";
 
+//Types
+import { Metadata } from "next";
+
 export default function Contact() {
   return (
     <main className="mx-[170px] pl-4 text-white flex pt-4">
@@ -15,3 +18,26 @@ export default function Contact() {
     </main>
   );
 }
+
+
+export const metadata: Metadata = {
+  title: "Contact | Ben Rodger - Full-stack Developer",
+  description:
+    "Hi, I'm Ben. A Full-stack Developer with 2+ years of experience",
+  keywords: [
+    "ben",
+    "ben rodger",
+    "breon",
+    "developer",
+    "fullstack",
+    "portfolio",
+    "resume",
+    "cv",
+  ],
+  openGraph: {
+    siteName: "Ben - Full-stack Developer",
+    title: "Search",
+    description:
+      "Hi, I'm Ben. A Fullstack Developer with 2+ years of experience",
+  },
+};
--- app/(main)/games/page.tsx
+++ app/(main)/games/page.tsx
@@ -1,9 +1,35 @@
 //components
-import { GoogleGameTile } from "@/components/google-components/google-games/google-gametile"
+import { GoogleGameTile } from "@/components/google-components/google-games/google-gametile";
 
+//Types
+import { Metadata } from "next";
 
 export default function Games() {
-  return <main className="mx-[170px] pl-4 text-white flex gap-20 pt-4">
-    <GoogleGameTile></GoogleGameTile>
-  </main>
+  return (
+    <main className="mx-[170px] pl-4 text-white flex gap-20 pt-4">
+      <GoogleGameTile></GoogleGameTile>
+    </main>
+  );
 }
+
+export const metadata: Metadata = {
+  title: "Games | Ben Rodger - Full-stack Developer",
+  description:
+    "Hi, I'm Ben. A Full-stack Developer with 2+ years of experience",
+  keywords: [
+    "ben",
+    "ben rodger",
+    "breon",
+    "developer",
+    "fullstack",
+    "portfolio",
+    "resume",
+    "cv",
+  ],
+  openGraph: {
+    siteName: "Ben - Full-stack Developer",
+    title: "Search",
+    description:
+      "Hi, I'm Ben. A Fullstack Developer with 2+ years of experience",
+  },
+};
